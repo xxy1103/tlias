0bda8e36d8b9f18d2bd625dfc1d5befcf5d940ed
xxy <xianxingyuan520@outlook.com>
2025-09-14 17:01:52 +0800

    登录的数据库实现

--- src/main/java/top/ulna520/tlias/service/UserService.java
+++ src/main/java/top/ulna520/tlias/service/UserService.java
@@ -1,19 +1,27 @@
 package top.ulna520.tlias.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import top.ulna520.tlias.mapper.UserMapper;
 import top.ulna520.tlias.model.User;
+import top.ulna520.tlias.util.JwtUttil;
 
-import java.util.Date;
-import java.util.List;
+import javax.crypto.SecretKey;
+import java.util.*;
 
+@Slf4j
 @Service
 public class UserService {
 
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private JwtUttil jwtUttil;
 
     public List<User> selectWithCondition(String name, Integer gender, Date begin, Date end, Integer page, Integer pageSize){
         int offset = (page - 1) * pageSize;
@@ -38,5 +46,16 @@ public class UserService {
     public void updateUser(User user) {
         user.setUpdateTime(new Date());
         userMapper.update(user);
+    }
+
+    public String login(User user) {
+        User quryUser = userMapper.getByUsernameAndPassword(user);
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", quryUser.getId());
+        claims.put("username", quryUser.getUsername());
+
+        String jwt = jwtUttil.constructJwt(claims);
+        log.info(jwt);
+        return jwt;
     }
 }
